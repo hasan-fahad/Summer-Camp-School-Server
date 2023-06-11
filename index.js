70930feb73ca9896d2d4e92109db86bfdba2385e
Fahad Hasan <66886981+hasan-fahad@users.noreply.github.com>
2023-06-11 18:04:09 +0600

    user&pass

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
@@ -10,8 +11,12 @@ app.use(cors());
 app.use(express.json());
 
 
+
 const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rpuxevk.mongodb.net/?retryWrites=true&w=majority`;
+
+// const uri = "mongodb+srv://schoolUser:LMtJvjOu8lHMju8L@cluster0.rpuxevk.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
